3464a46dc3e261e3218dde7d3c0625ddf756eab2
Ashish Mishra <ashish.mishra.sse@gmail.com>
2025-02-27 09:18:21 +0530

    DataTypes_StringConcatenation_MathOperations

---
+++ src/day6/VariableConcept.java
@@ -0,0 +1,37 @@
+package day6;
+
+public class VariableConcept {
+
+    public static void main(String[] args) {
+
+        //Variables: are like jars, boxes, containers which is capable of holding something
+        //How to create variables
+
+        //1. dataType variableName=value;
+        // -128 to 127
+        byte x = -128;
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+
+        //-32,768 to 32,767
+        short y = 32767;
+
+        //-2,147,483,648 to 2,147,483,647
+        int z = 214748364;
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+
+        //-9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+
+        long a = 2147483648l;
+
+        //2. dataType variableName
+        //   variableName=value;
+
+
+        //1 byte = 8 bits
+    }
+}
+
+
+
