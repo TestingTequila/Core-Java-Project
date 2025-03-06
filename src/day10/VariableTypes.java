19a7b19364952a4fa4ece43c6c95122eac55274a
Ashish Mishra <ashish.mishra.sse@gmail.com>
2025-03-06 11:53:13 +0530

    Arrays

---
+++ src/day10/VariableTypes.java
@@ -0,0 +1,21 @@
+package day10;
+
+public class VariableTypes {
+    public static void main(String[] args) {
+        int x = 10;
+
+        int y;
+        y = 27;
+
+        int a = 12;
+        int b = 34;
+        int c = 56;
+        int d = 27;
+        int e = 89;
+    }
+
+}
+
+//12, 34, 56,78,89
+
+
