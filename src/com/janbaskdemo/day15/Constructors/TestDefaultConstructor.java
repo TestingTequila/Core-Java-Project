6ec0103e9b2c4789663e88a787f39ed54ecc6776
Ashish Mishra <ashish.mishra.sse@gmail.com>
2025-03-19 12:24:25 +0530

    Deleted day15 topics

--- src/com/janbaskdemo/day15/Constructors/TestDefaultConstructor.java
+++
@@ -1,8 +0,0 @@
-package com.janbaskdemo.day15.Constructors;
-
-public class TestDefaultConstructor
-{
-    public static void main(String[] args) {
-        WebApplication wa = new WebApplication();
-    }
-}
